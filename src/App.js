002ffbe2f46b22ce23028b7c7d6e4a106a2dfa54
Tanveer Ahmad <tanveerisonline@gmail.com>
2023-01-18 19:05:13 +0530

    create todo app in react with local storage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+import TodoContainer from "./components/TodoContainer";
+function App() {
+  return (
+    <div>
+      <TodoContainer />
+
+    </div>
+  )
+}
+
+export default App
